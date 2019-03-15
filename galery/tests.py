0de0c43f7cb4a10348b5dbe903f323483df152b4
Wihogora Wivine <wwihogora@gmail.com>
2019-03-15 17:36:55 +0200

    Add class locationtest in test.py

--- galery/tests.py
+++ galery/tests.py
@@ -9,3 +9,38 @@ class CategoryTestCase(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.food, Category))
+
+    def tearDown(self):
+        Category.objects.all().delete()
+
+    def test_save_category(self):
+        self.food.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_category(self):
+        self.food.delete_category('Food')
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) == 0)
+
+
+class LocationTestCase(TestCase):
+    def setUp(self):
+        self.kigali = Location(name = 'Kigali')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kigali, Location))
+
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_save_location(self):
+        self.kigali.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_delete_location(self, event = None):
+        self.kigali.delete_location('Kigali')
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) == 0)
+
