649b6e68a3d8353c7935f0d09a9a3b73cf53734c
Wihogora Wivine <wwihogora@gmail.com>
2019-03-15 13:06:34 +0200

    Add class category models.py

--- galery/models.py
+++ galery/models.py
@@ -3,6 +3,29 @@ import datetime as dt
 from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
+
+class Category(models.Model):
+    photo_category = models.CharField(max_length=50)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+    
+    def update_category(self, update):
+        self.photo_category = update
+        self.save()
+
+    @classmethod
+    def get_category_id(cls, id):
+        category = Category.objects.get(pk = id)
+        return category
+
+    def __str__(self):
+        return self.photo_category
+
+
 class Location(models.Model):
     photo_location = models.CharField(max_length=50)
 
