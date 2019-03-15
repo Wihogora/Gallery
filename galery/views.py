5e7c641051c55d7658ef9ba509b2e6f5bd3e5e06
Wihogora Wivine <wwihogora@gmail.com>
2019-03-15 17:49:08 +0200

    Add function of displaylocation in views.py

--- galery/views.py
+++ galery/views.py
@@ -1,38 +1,21 @@
-import datetime as dt
-from django.http  import HttpResponse,Http404
+# import datetime as dt
+from django.http  import Http404
 from django.shortcuts import render,redirect
+from .models import Image,Category,Location
 
 #..........
 
-def welcome(request):
-    return render(request, 'welcome.html')
+def index(request):
+    images = Image.objects.all()
+    locations = Location.objects.all()
+    return render(request,'index.html',{'images':images,'locations':locations})
 
-def galery_of_day(request):
-    date = dt.date.today()
-    return render(request, 'all_galery/today_galery.html', {"date": date,})
+def display_location(request,location_id):
+    try:
+        location = Location.objects.get(id = location_id)
+        images = Image.objects.filter(image_location = location.id)
+    except:
+        raise Http404()
+    return render(request,'location.html',{'location':location,'images':images})
 
-def convert_dates(dates):
 
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-
-def past_days_galery(request,past_date):
-        try:
-        # Converts data from the string Url
-            date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
-        except ValueError:
-        # Raise 404 error when ValueError is thrown
-            raise Http404()
-            assert False
-
-        if date == dt.date.today():
-            return redirect(galery_of_day)
-
-        return render(request, 'all_galery/today_galery.html', {"date": date})
