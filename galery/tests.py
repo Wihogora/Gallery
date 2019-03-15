7eb7f016739d558a89461d004c98d98b9bac6948
Wihogora Wivine <wwihogora@gmail.com>
2019-03-15 17:07:48 +0200

    Add class categorytest in test.py

--- galery/tests.py
+++ galery/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Image,Category,Location
 
 # Create your tests here.
+class CategoryTestCase(TestCase):
+
+    def setUp(self):
+        self.food = Category(name = 'Food')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food, Category))
