2645c327f2cf43857dcf5afe250a0d5fd6da8f3a
Wihogora Wivine <wwihogora@gmail.com>
2019-03-15 17:43:13 +0200

    Add url for search in urls.py

--- galery/urls.py
+++ galery/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
 #.........
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_galery,name = 'pastGalery')
+    url(r'^$',views.index,name='index'),
+    url(r'^location/(\d+)',views.display_location,name='displayLocation'),
+    url(r'^search/',views.search_category,name='search_category'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
