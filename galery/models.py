87a73f59fb19032a3b9c3eb93f10e3f13ed59e7e
Wihogora Wivine <wwihogora@gmail.com>
2019-03-15 13:02:04 +0200

    Add class location models.py

--- galery/models.py
+++ galery/models.py
@@ -1,9 +1,25 @@
 from django.db import models
+import datetime as dt
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
+class Location(models.Model):
+    photo_location = models.CharField(max_length=50)
+
+    def save_location(self):
+        self.save()
+    
+    def delete_location(self):
+        self.delete()
+    
+    def update_location(self, update):
+        self.photo_location = update
+        self.save()
+    
+    @classmethod
+    def get_location_id(cls, id):
+        locate = Location.objects.get(pk = id)
+        return locate
+
     def __str__(self):
-        return self.first_name
+        return self.photo_location
